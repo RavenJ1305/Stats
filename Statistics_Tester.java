51182434cbe519d54e9308e181f67f158fcf4e65
RavenJ1305 <ravenj@go.stockton.edu>
2022-02-04 21:39:51 -0500

    9:39p

--- Statistics_Tester.java
+++ Statistics_Tester.java
@@ -1,8 +1,7 @@
 public class Statistics_Tester {
     public static void main(String args[]){
         Statistics s1 = new Statistics();
-        int list[] = {1, 5, 3, 5, 3, 5};
-
-        s1.result(list);
+        int listA[] = {1, 5, 3, 5, 3, 5};
+        s1.result(listA);
     }
 }
