e6ba873c4db65950954adfac6bbc68a3b8072bee
RavenJ1305 <ravenj@go.stockton.edu>
2022-02-10 23:48:47 -0500

    11:48p

---
+++ Project_1/CSV_Writer/CSV_Writer_Tester.java
@@ -0,0 +1,8 @@
+package CSV_Writer;
+
+public class CSV_Writer_Tester {
+    public static void main(String[] args){
+        CSV_Writer writer = new CSV_Writer();
+        writer.writeData();
+    }
+}
