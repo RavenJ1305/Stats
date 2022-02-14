aa5f551312ea93f0b04ec2e31b44584bd2683d58
RavenJ1305 <ravenj@go.stockton.edu>
2022-02-14 00:06:41 -0500

    1206a

--- Statistics_Programs/Statistics_Tester.java
+++ Statistics_Programs/Statistics_Tester.java
@@ -1,10 +1,20 @@
+import java.util.ArrayList;
+
 public class Statistics_Tester {
     public static void main(String args[]){
         Statistics s1 = new Statistics();
-        Statistics s2 = new Statistics();
         int listA[] = {1, 5, 3, 5, 3, 5};
         int listB[] = {1, 2, 3, 4, 5, 6};
         s1.result(listA);
-        s2.result2(listA, listB);
+        s1.result2(listA, listB);
+        ArrayList listC = new ArrayList();
+        listC.add(1);
+        listC.add(3);
+        listC.add(5);
+        ArrayList listD = new ArrayList();
+        listD.add(2);
+        listD.add(4);
+        listD.add(6);
+        s1.union(listC, listD);
     }
 }
