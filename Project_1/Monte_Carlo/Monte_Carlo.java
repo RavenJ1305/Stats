5da7a391eba5fad5c073424589947348058d5df0
RavenJ1305 <ravenj@go.stockton.edu>
2022-02-14 00:43:10 -0500

    1243a

--- Project_1/Monte_Carlo/Monte_Carlo.java
+++ Project_1/Monte_Carlo/Monte_Carlo.java
@@ -1,5 +1,50 @@
 package Monte_Carlo;
+import java.util.*;
+import java.io.*;
 
 public class Monte_Carlo {
+    Random rand = new Random();
+    int count = 0;
+    int playerDoor = 0;
+    int winDoor = 0;
     
+    public double dontChange(double  plays){
+        count = 0;
+        for(int i = 1; i <= plays; i++){
+            playerDoor = rand.nextInt(3);
+            winDoor = rand.nextInt(3);
+
+            if(playerDoor == winDoor){
+                count++;
+            }
+        }
+
+        return (count/plays) * 100;
+    }
+
+    public double doChange(double plays){
+        count = 0;
+        for(int i = 1; i <= plays; i++){
+            playerDoor = rand.nextInt(3);
+            winDoor = rand.nextInt(3);
+            int wrongDoor = winDoor;
+            
+            while(wrongDoor == winDoor || wrongDoor == playerDoor){
+                wrongDoor = rand.nextInt(3);
+            }
+
+            int changeDoor = 3 - (playerDoor + wrongDoor);
+
+            if(changeDoor == winDoor){
+                count++;
+            }
+        }
+
+        return (count/plays) * 100;
+    }
+
+    public void run(double plays){
+        System.out.println("If the player does not change the door every time out of " + plays + " they will win " + dontChange(plays) + "% of the time.");
+        System.out.println("If the player does change the door every time out of " + plays + " they will win " + doChange(plays) + "% of the time.");
+    }
 }
---
+++ Project_1/Monte_Carlo/Monte_Tester.java
@@ -0,0 +1,8 @@
+package Monte_Carlo;
+
+public class Monte_Tester {
+    public static void main(String[] args){
+        Monte_Carlo monte = new Monte_Carlo();
+        monte.run(1000000);
+    }
+}
