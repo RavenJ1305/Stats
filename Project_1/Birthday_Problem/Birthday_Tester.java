7039b925c3a24c194f88518ffa061ee5fa792cbd
RavenJ1305 <ravenj@go.stockton.edu>
2022-02-15 16:13:40 -0500

    413p

--- Project_1/Birthday_Problem/Birthday_Tester.java
+++ Project_1/Birthday_Problem/Birthday_Tester.java
@@ -4,7 +4,7 @@ package Birthday_Problem;
 public class Birthday_Tester {
     public static void main(String[] args){
         Birthday bdayComp = new Birthday();
-       bdayComp.result(23, 10000);
+        bdayComp.result(23, 10000);
     }
 
 }
