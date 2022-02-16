4e64fa043a8d02e6f558141fff1f05bed278c00d
RavenJ1305 <ravenj@go.stockton.edu>
2022-02-16 00:54:35 -0500

    1254p

--- Project_1/Birthday_Problem/Birthday_Tester.java
+++ Project_1/Birthday_Problem/Birthday_Tester.java
@@ -3,7 +3,9 @@ package Birthday_Problem;
 
 public class Birthday_Tester {
     public static void main(String[] args){
+        //Returns the probability two people will share a birthday in a room of X amount of people 
         Birthday bdayComp = new Birthday();
+        //Adjust the parameters for the amount of people and the amount of times the code will run
         bdayComp.result(23, 10000);
     }
 
--- Project_1/Birthday_Problem/Person.java
+++ Project_1/Birthday_Problem/Person.java
@@ -9,7 +9,8 @@ public class Person {
         return bday;
     }
 
+    //Sets person's birthday to a day of the year
     public Person(){
-        bday = rand.nextInt(365) + 1;
+        bday = rand.nextInt(365) + 1; //+ 1 to account for the random int giving 0
     }
 }
