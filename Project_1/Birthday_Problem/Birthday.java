0b2b720a17758c833d1937ca00114b36adad84f1
RavenJ1305 <ravenj@go.stockton.edu>
2022-02-16 00:48:16 -0500

    1248a

--- Project_1/Birthday_Problem/Birthday.java
+++ Project_1/Birthday_Problem/Birthday.java
@@ -4,16 +4,21 @@ public class Birthday {
     private int counter;
     private double result;
 
+    //Loops through as many times as specified by the runAmnt parameter and returns the probability
     public void result(int pplAmnt, int runAmnt){
+        
         for(int i=0;i < runAmnt; i++){
             Person[] room = new Person[pplAmnt];
+            //The room variable gets populated by the classRoom method's returned Array
             room = classRoom(pplAmnt);
+            //If 1 is returned by compareBday it's added to the result variable to calculate the probability
             result += compareBday(room);
         }
         result = (result/runAmnt) * 100;
         System.out.println("The probability of two people sharing the same birthday in a room of " + pplAmnt + " people is: " + result + "%");
     }
 
+    //Creates a specified amount of person objects and puts them into an array based off of the pplAmnt parameter and returns the Array
     public Person[] classRoom(int pplAmnt){
         Person[] room = new Person[pplAmnt];
 
@@ -25,6 +30,7 @@ public class Birthday {
         return room;
     }
 
+    //Loops through the array to find a matching birthday, once found adds to counter, breaks the loop, checks if counter was incremented, breaks the loop if so, returns the counter
     public int compareBday(Person[] room){
         counter = 0;
         for(int i=0; i < room.length; i++){
